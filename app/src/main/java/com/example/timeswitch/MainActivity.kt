dc51756c722119fc14c828504bfc7c3a5f881941
Core i7 <mamajonovyosir3@gmail.com>
2024-12-10 20:48:24 +0500

    Time Switch

---
+++ app/src/main/java/com/example/timeswitch/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.timeswitch
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+}
